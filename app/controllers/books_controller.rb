49ff9be3a14c263532ad89514bfadd91ed77907a
EC2 Default User <ec2-user@ip-172-31-15-49.ec2.internal>
2022-04-17 04:28:35 +0000

    [Add]create,update成功時にメッセージを表示する機能を実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), flash: { notice: 'Book was successfully created.' }
     else
       render :index
     end
@@ -27,7 +27,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)
+    redirect_to book_path(book.id), flash: { notice: 'Book was successfully updated.' }
   end
 
   def destroy
