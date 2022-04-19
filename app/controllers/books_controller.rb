d54f6aa66f4200e422fd2ee22d7b4cac4ee0133d
EC2 Default User <ec2-user@ip-172-31-15-49.ec2.internal>
2022-04-19 01:29:58 +0000

    [Update]index,edtiのエラー表示のcssを調整

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -38,7 +38,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to "/books"
+    redirect_to "/books", flash: { notice: "Book was successfully destroyed."}
   end
 
   private
